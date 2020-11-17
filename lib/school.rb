3113176af56370aab5d7d3d368726a90ae93c25b
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-11-17 17:05:36 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+
+
+
+class School
+  attr_accessor :roster
+  def initialize(roster)
+    @roster = {}
+  end
+
+
+  def add_student(name, grade)
+    self.roster[grade] ||= []
+    self.roster[grade] << name
+  end
+
+  def grade(number)
+    self.roster[number]
+  end
+
+  def sort
+    new_hash = {}
+    self.roster.keys.sort.each do |key|
+      new_hash[key] = self.roster[key].sort
+    end
+    new_hash
+  end
+
+end
+
